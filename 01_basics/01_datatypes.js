038e2dad979387246fa0352e01eda9549dee6e21
Harshhh2627 <110217593+Harshhh2627@users.noreply.github.com>
2025-07-10 10:52:46 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+let name = "Harsh"
+let age =  22
+let isAdult  = true
+
+// number = s to the power 53
+// bigint
+//string  = Harsh
+//boolean
+//null
+//undefined
+//symbol
+
+console.log(typeof undefined) // is undefined
+console.log(typeof null); //is object
