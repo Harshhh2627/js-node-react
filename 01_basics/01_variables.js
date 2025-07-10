f55055fb215188a97b6329fb7e9db6d023595aba
Harshhh2627 <110217593+Harshhh2627@users.noreply.github.com>
2025-07-10 09:54:27 +0000

    complted

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =2627
+let accountEmail = "Harsh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState
+
+
+// accountId = 2
+accountEmail = "Mistry@gmail.com"
+accountPassword = "23456"
+accountCity = "Pune"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+var is not to be used because of scope issues.
+*/
